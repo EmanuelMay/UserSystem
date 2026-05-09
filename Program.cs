0622fb6d43698eeb1452df64d8e2c0362546417f
Emanuel May <ema.luis.may@proton.me>
2026-05-08 22:55:40 -0300

    Ajustando a injecao de dependencia para o GetAll

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using UserSystem.Application.Services;
+using UserSystem.Domain.Interfaces;
 using UserSystem.Infrastructure.Context;
+using UserSystem.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 string? mySqlConnection = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddDbContext<AppDbContext>(options =>
